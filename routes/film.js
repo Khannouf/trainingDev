073b63d7b4c0035a3fc203e8c7fa2a288e43f85c
Khannouf <lkhannouf@gmail.com>
2023-11-14 16:27:32 +0100

    -commentaire

--- routes/film.js
+++ routes/film.js
@@ -21,17 +21,13 @@ router.get("/:id", (req, res) =>{
 router.post('/create', (req, res) => {
   try {
     const { nom, description, date_parution, note } = req.body;
-    console.log(req.body);
 
-    // Assurez-vous que toutes les données nécessaires sont présentes dans la requête
     if (!nom || !description || !date_parution) {
       return res.status(404).json({ message: 'Nom, description et date de parution sont requis.' });
     }
 
-    // Exécutez la requête SQL pour insérer le nouveau film dans la table
     const result =  connection.query('INSERT INTO film (nom, description, date_parution, note) VALUES (?, ?, ?, ?)', [nom, description, date_parution, note]);
 
-    // Envoyez une réponse réussie
     res.status(201).json({ message: 'Film ajouté avec succès', insertId: result.insertId });
   } catch (error) {
     console.error('Erreur lors de l\'ajout du film :', error);
@@ -44,14 +40,12 @@ router.put('/update/:id', (req, res) => {
     const { nom, description, date_parution, note } = req.body;
     const filmId = req.params.id;
 
-    // Assurez-vous que toutes les données nécessaires sont présentes dans la requête
     if (!nom || !description || !date_parution) {
       return res.status(404).json({ message: 'Nom, description et date de parution sont requis.' });
     }
     
     connection.query("SELECT * FROM film WHERE id =" +filmId, (err, rows, fields) => {
       if(rows.length !== 0){
-         // Exécutez la requête SQL pour mettre à jour le film dans la table
         const result =  connection.query(
           'UPDATE film SET nom = ?, description = ?, date_parution = ?, note = ? WHERE id = ?',
           [nom, description, date_parution, note, filmId]
@@ -81,10 +75,6 @@ router.delete('/delete/:id', (req, res) => {
         res.status(404).json({ message: 'Film non trouvé avec l\'ID spécifié' });
       }
     });
-    // Exécutez la requête SQL pour supprimer le film dans la table
-    // const result = connection.query('DELETE FROM film WHERE id = ?', [filmId]);
-
-    //   res.status(200).json({ message: 'Film supprimé avec succès' });
   } catch (error) {
     console.error('Erreur lors de la suppression du film :', error);
     
